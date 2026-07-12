50e9bf6f9bf6b84cbd258ae623872d27abac4f7c
Calvin Zhou <calzh6@gmail.com>
2026-07-12 12:17:01 -0400

    Solve make-diagonal

--- make-diagonal/make-diagonal.py
+++ make-diagonal/make-diagonal.py
@@ -1,10 +1,4 @@
 import numpy as np
 
 def make_diagonal(v):
-    A = np.zeros((len(v), len(v)))
-    print(A)
-    N, M = A.shape
-    for i in range(N):
-        A[i, i] = v[i]
-
-    return A
+    return np.diag(np.asarray(v))
