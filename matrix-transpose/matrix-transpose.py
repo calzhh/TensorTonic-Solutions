d02c2e7290fddc85dd62c8969086d0977aea77e6
Calvin Zhou <calzh6@gmail.com>
2026-07-11 22:56:47 -0400

    Solve matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 def matrix_transpose(A):
-    B = np.zeros((len(A[0]), len(A)))
-    for i in range(len(A)):
-        for j in range(len(B)):
-            B[j][i] = A[i][j]
+    A = np.asarray(A)
+    N, M = A.shape
+    B = np.empty((M, N), dtype=A.dtype)
+
+    for i in range(N):
+        B[:, i] = A[i, :]
 
     return B
